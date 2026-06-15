3a2fc1668f6f99c6f57bad4d729bc6ad5f8d684c
June Kelly <june@getsona.com>
2026-06-15 14:39:21 +0100

    Set up claude paths properly

--- setup/install.sh
+++ setup/install.sh
@@ -4,4 +4,5 @@ mkdir -p ~/tools
 
 git clone https://github.com/JuneKelly/co-review.git ~/tools/co-review
 
+mkdir -p ~/.claude/commands
 ln -s ~/tools/commands/co-review.md ~/.claude/commands/co-review.md
